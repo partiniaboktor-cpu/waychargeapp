5e6b574d7c14ab58225c6cc2135f6b704be04425
Pattou Emad <pattouemad@Partinias-MacBook.local>
2026-04-17 18:10:02 +0200

    finished request confirmation page

--- src/Pages/Requestconfirmation.jsx
+++ src/Pages/Requestconfirmation.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './Requestconfirmation.css'
 import Upper from '../Components/Upper';
 import tick from '../Assets/tick.svg'
+import Button from '../Components/Button';
 
 const Requestconfirmation = () => {
     return ( <>
@@ -9,13 +10,14 @@ const Requestconfirmation = () => {
     
     <div className='container25'>
     <img className='tick' src={tick} alt="tick" />
-    
-    
+    <h1 className='message25'>Your Request has been requested</h1>
+    <p className='description25'>Your request has been confirmed. Thank you for<br></br> choosing us! We look forward to seeing you again <br></br>soon with WayCharge ⚡</p>
 
 
+<Button word='Go to Home' />
 
     </div>
     </> );
 }
  
-export default R=Requestconfirmation;
+export default Requestconfirmation;
