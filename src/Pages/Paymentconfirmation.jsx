c5f543d7e349c384991e62100411e13fc4663b73
Pattou Emad <pattouemad@Partinias-MacBook.local>
2026-04-17 18:11:23 +0200

    create payment confirmation page and set it up

---
+++ src/Pages/Paymentconfirmation.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Paymentconfirmation.css'
+import Upper from '../Components/Upper';
+
+const Paymentconfirmation = () => {
+    return ( <>
+    
+    
+    <Upper />
+    
+    
+    
+    
+    </> );
+}
+ 
+export default Paymentconfirmation;
