4b359f915268feee8c23eac6d0e4d8245f4233ff
Pattou Emad <pattouemad@Partinias-MacBook.local>
2026-04-17 18:13:12 +0200

    changed the names in diffrent 2 pages

--- src/Pages/Paymentconfirmation.jsx
+++ src/Pages/Paymentconfirmation.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './Paymentconfirmation.css'
 import Upper from '../Components/Upper';
+import tick from '../Assets/tick.svg'
+import Button from '../Components/Button';
 
 const Paymentconfirmation = () => {
     return ( <>
@@ -8,7 +10,15 @@ const Paymentconfirmation = () => {
     
     <Upper />
     
-    
+    <div className='container25'>
+    <img className='tick' src={tick} alt="tick" />
+    <h1 className='message25'>Payment Confirmed</h1>
+    <p className='description25'>Your Payment has been confirmed. Thank you for<br></br> choosing us! We look forward to seeing you again <br></br>soon with WayCharge ⚡</p>
+
+
+     <Button word='Go to Home' />
+
+    </div>
     
     
     </> );
